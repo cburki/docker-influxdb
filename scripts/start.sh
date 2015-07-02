d92b631a61247cdc84e5d8639b9a56c28c7f4d69
Christophe Burki <christophe.burki@gmail.com>
2015-07-02 21:25:43 +0200

    Fixed for allowing upgrade without rebuilding data volume container

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,17 +1,31 @@
 #!/bin/bash
 
 VAR_DIR=/data/var
+ETC_DIR=/data/etc
+RUN_DIR=$VAR_DIR/run
 OPT_DIR=$VAR_DIR/opt
 STATE_DIR=$VAR_DIR/state
-INFLUXDB_DIR=$OPT_DIR/influxdb
-INFLUXDB_DATADIR=$INFLUXDB_DIR/data
-INFLUXDB_METADIR=$INFLUXDB_DIR/meta
-INFLUXDB_HHDIR=$INFLUXDB_DIR/influxdb/hh
+INFLUXDB_ETCDIR=$ETC_DIR/influxdb
+INFLUXDB_RUNDIR=$RUN_DIR/influxdb
+INFLUXDB_OPTDIR=$OPT_DIR/influxdb
+INFLUXDB_DATADIR=$INFLUXDB_OPTDIR/data
+INFLUXDB_METADIR=$INFLUXDB_OPTDIR/meta
+INFLUXDB_HHDIR=$INFLUXDB_OPTDIR/influxdb/hh
 
 # Setup data directories
 if [ ! -d $VAR_DIR ]; then
     echo "Creating directory $VAR_DIR"
     mkdir -p $VAR_DIR
+fi
+
+if [ ! -d $ETC_DIR ]; then
+    echo "Creating directory $ETC_DIR"
+    mkdir -p $ETC_DIR
+fi
+
+if [ ! -d $RUN_DIR ]; then
+    echo "Creating directory $RUN_DIR"
+    mkdir -p $RUN_DIR
 fi
 
 if [ ! -d $OPT_DIR ]; then
@@ -24,27 +38,37 @@ if [ ! -d $STATE_DIR ]; then
     mkdir -p $STATE_DIR
 fi
 
+if [ ! -d $INFLUXDB_ETCDIR ]; then
+    echo "Creating directory $INFLUXDB_ETCDIR"
+    mkdir -p $INFLUXDB_ETCDIR
+fi
+
+if [ ! -d $INFLUXDB_RUNDIR ]; then
+    echo "Creating directory $INFLUXDB_RUNDIR"
+    mkdir -p $INFLUXDB_RUNDIR
+fi
+
 if [ ! -d $INFLUXDB_DATADIR ]; then
-    echo "Creating directory $INFLUX_DATADIR"
-    mkdir -p $INLUXDB_DATADIR
+    echo "Creating directory $INFLUXDB_DATADIR"
+    mkdir -p $INFLUXDB_DATADIR
 fi
 
 if [ ! -d $INFLUXDB_METADIR ]; then
-    echo "Creating directory $INFLUX_METADIR"
-    mkdir -p $INLUXDB_METADIR
+    echo "Creating directory $INFLUXDB_METADIR"
+    mkdir -p $INFLUXDB_METADIR
 fi
 
 if [ ! -d $INFLUXDB_HHDIR ]; then
-    echo "Creating directory $INFLUX_HHDIR"
-    mkdir -p $INLUXDB_HHDIR
+    echo "Creating directory $INFLUXDB_HHDIR"
+    mkdir -p $INFLUXDB_HHDIR
 fi
 
 echo $INFLUXDB_DATADIR > $STATE_DIR/influxdb-datadir.txt
 
 if [ ! -f $STATE_DIR/influxdb.initialized ]; then
 
-    CONFFILE=/etc/opt/influxdb/influxdb.conf
-    PIDFILE=/var/run/influxdb/influxdb.pid
+    CONFFILE=$INFLUXDB_ETCDIR/influxdb.conf
+    PIDFILE=$INFLUXDB_RUNDIR/influxdb.pid
     mv /tmp/influxdb-noauth.conf $CONFFILE
     
     echo "Starting influxdb"
@@ -70,12 +94,13 @@ if [ ! -f $STATE_DIR/influxdb.initialized ]; then
     kill `cat $PIDFILE`
     echo "done" > $STATE_DIR/influxdb.initialized
     rm -f $PIDFILE
-
-    chown -R influxdb:influxdb $INFLUXDB_DIR
-    chown -R influxdb:influxdb /opt/influxdb
-    chown -R influxdb:influxdb /var/opt/influxdb
-    chown -R influxdb:influxdb /var/run/influxdb
 fi
+
+# Set influxdb permissions.
+chown -R influxdb:influxdb $INFLUXDB_OPTDIR
+chown -R influxdb:influxdb $INFLUXDB_RUNDIR
+chown -R influxdb:influxdb /opt/influxdb
+chown -R influxdb:influxdb /var/opt/influxdb
 
 echo "Starting supervisord"
 /usr/local/bin/supervisord -c /etc/supervisord.conf --loglevel=debug -n
